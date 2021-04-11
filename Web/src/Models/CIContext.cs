7fad8acf45fbe4f8dd4d8fd579729b8a6a336765
JasonKim2020 <whmarti@hotmail.com>
2021-04-12 08:18:10 +1200

    First version

--- Web/src/Models/CIContext.cs
+++ Web/src/Models/CIContext.cs
@@ -30,7 +30,7 @@ namespace UnivWebApp.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=CI;Trusted_Connection=True;");
+                //optionsBuilder.UseSqlServer("Server=localhost;Database=CI;Uid=sa;Pwd=wh;");
             }
         }
 
